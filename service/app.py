ac3f50d9c4273a622dfb887b1e2c647b9856557f
Grayson Koonce <grayson@uber.com>
2015-11-09 21:22:26 -0800

    add hostname to service response

--- service/app.py
+++ service/app.py
@@ -1,15 +1,22 @@
+import socket
+
 from flask import Flask
 from redis import Redis
 
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
+hostname = socket.gethostname()
 
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+    return 'This is %s! There have been %s total hits.' % (
+        hostname,
+        redis.get('hits'),
+    )
 
 
 if __name__ == "__main__":
